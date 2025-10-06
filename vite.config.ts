6848a714e9058ff7f2682e704dd79249baf880ea
Josiah Glosson <soujournme@gmail.com>
2025-10-06 00:04:31 -0500

    Get rid of singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { viteSingleFile } from 'vite-plugin-singlefile'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), nodePolyfills({ include: ['buffer'] }), viteSingleFile()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
