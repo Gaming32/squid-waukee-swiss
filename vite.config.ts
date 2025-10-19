f9bfde44942b927db7ae804cc884f248794403b1
Josiah Glosson <soujournme@gmail.com>
2025-10-19 09:50:45 -0500

    Update tournament-manager and remove buffer polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { qrcode } from 'vite-plugin-qrcode'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [vue(), vueDevTools(), qrcode(), nodePolyfills({ include: ['buffer'] })],
+  plugins: [vue(), vueDevTools(), qrcode()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
