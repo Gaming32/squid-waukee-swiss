5e1691d381f904ca89c131d3eec261d9ea7d08d0
Josiah Glosson <soujournme@gmail.com>
2025-10-27 15:24:02 -0500

    Add a page footer and Vercel stuff (#2)
    * Add a footer
    
    * Log process env on build
    
    * Use in to check for VERCEL_GIT_PULL_REQUEST_ID
    
    * Log git remotes in build
    
    * Vercel-specific GIT_REMOTE
    
    * Remove debug code
    
    * Use VERCEL_PROJECT_PRODUCTION_URL instead of hardcoded URL
    
    * Fix APP_URL

---
+++ src/components/RepoFooter.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const gitRemote: string = import.meta.env.GIT_REMOTE
+const gitDirty = Boolean(import.meta.env.GIT_DIRTY)
+const gitCommit: string = import.meta.env.GIT_COMMIT
+const gitPr: string = import.meta.env.GIT_PR
+</script>
+
+<template>
+  <p class="footer">
+    ©2025 Gaming32. Commit
+    <a v-if="gitRemote" :href="`${gitRemote}/commit/${gitCommit}`">{{ gitCommit }}</a>
+    <template v-else>{{ gitCommit }}</template
+    >.
+    <template v-if="gitPr"
+      >(Pull request <a :href="`${gitRemote}/pull/${gitPr}`">#{{ gitPr }}</a
+      >)</template
+    >
+    <template v-if="gitDirty">(DIRTY)</template>
+  </p>
+</template>
+
+<style scoped>
+.footer {
+  text-align: center;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,40 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { qrcode } from 'vite-plugin-qrcode'
+import describe from 'git-describe'
+import { spawnSync } from 'node:child_process'
+
+const extraEnv: { [key: string]: string } = {}
+
+if (process.env.VERCEL_GIT_PROVIDER === 'github') {
+  extraEnv.GIT_REMOTE = `https://github.com/${process.env.VERCEL_GIT_REPO_OWNER}/${process.env.VERCEL_GIT_REPO_SLUG}`
+} else {
+  try {
+    extraEnv.GIT_REMOTE = spawnSync('git', ['remote', 'get-url', 'origin'])
+      .stdout.toString()
+      .trim()
+      .replace(/\.git$/, '')
+  } catch {}
+}
+
+if ('VERCEL_GIT_PULL_REQUEST_ID' in process.env) {
+  extraEnv.GIT_PR = process.env.VERCEL_GIT_PULL_REQUEST_ID!
+}
+
+try {
+  const gitInfo = describe.gitDescribeSync()
+  if (gitInfo.dirty) {
+    extraEnv.GIT_DIRTY = '1'
+  }
+  extraEnv.GIT_COMMIT = gitInfo.hash
+} catch {}
+
+extraEnv.APP_URL =
+  process.env.VERCEL_ENV === 'production'
+    ? process.env.VERCEL_PROJECT_PRODUCTION_URL!
+    : 'VERCEL_URL' in process.env
+      ? process.env.VERCEL_URL!
+      : 'squid-waukee-tourney.jemnetworks.com'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +48,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  define: Object.fromEntries(
+    Object.entries(extraEnv).map(([key, value]) => [
+      `import.meta.env.${key}`,
+      JSON.stringify(value),
+    ]),
+  ),
 })
